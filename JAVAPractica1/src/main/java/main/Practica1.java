2a3153fd61a9582de0d33fd602fafc3ef965d8ff
Dany LS <dadeleon89@gmail.com>
2024-08-07 11:22:12 -0600

    Archivo Inicial

---
+++ JAVAPractica1/src/main/java/main/Practica1.java
@@ -0,0 +1,15 @@
+
+package main;
+
+import FrontEnd.Pantalla;
+
+
+public class Practica1 {
+
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
